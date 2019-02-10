b6fda5edc01c1ae19f588dcaf27712d8c60b81e3
Benjamin Lu <benlumia007@gmail.com>
2019-02-09 23:56:08 -0800

    updated

--- index.php
+++ index.php
@@ -44,8 +44,10 @@ $data = $yaml->load( ( file_exists( '/vagrant/sandbox-custom.yml' ) ) ? '/vagran
 		</div>
 		<div class="sidebar">
 			<?php foreach ( $data['utilities'] as $name => $core ) {
-				if ( !empty( $core as $feature ) ) {
-					echo $feature;
+				if ( !empty( $core ) ) {
+					foreach ( $core as $feature ) {
+						echo $feature;
+					}
 				}
 			} ?>
 		</div>
