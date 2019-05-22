511a24aa6233db2bdddba5b7e519566664ae28f7
Benjamin Lu <benlumia007@gmail.com>
2019-05-22 11:05:56 -0700

    updated

--- includes/functions.php
+++ includes/functions.php
@@ -4,3 +4,19 @@ function endsWith( $haystack, $needle ) {
     return $length === 0 || 
     ( substr( $haystack, -$length ) === $needle );
 }
+
+function sites() {
+    foreach ( $data['sites'] as $name => $site ) { ?>
+        <div class="entry-content">
+            <span class="site-name"><?php echo strip_tags( $name ); ?></span>
+            <?php
+            if ( ! empty( $site['hosts'] ) ) {
+                foreach ( $site['hosts'] as $host ) { ?>
+                    <a class="link" href="<?php echo 'https://'.$host; ?>" target="_blank"><?php echo 'https://'.$host; ?></a>
+                    <?php
+                }
+            }
+            ?>
+        </div>
+    <?php }
+}
