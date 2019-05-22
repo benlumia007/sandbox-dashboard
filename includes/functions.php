7cc06408da3b5a46a248656af7b5b146c048dfde
Benjamin Lu <benlumia007@gmail.com>
2019-05-22 11:14:46 -0700

    updated

--- includes/functions.php
+++ includes/functions.php
@@ -8,7 +8,7 @@ function endsWith( $haystack, $needle ) {
 function sites() {
     $yaml = new Alchemy\Component\Yaml\Yaml();
     $data = $yaml->load( ( file_exists( '/vagrant/sandbox-custom.yml' ) ) ? '/vagrant/sandbox-custom.yml' : '/vagrant/sandbox-setup.yml' );
-    
+
     foreach ( $data['sites'] as $name => $site ) { ?>
         <div class="entry-content">
             <span class="site-name"><?php echo strip_tags( $name ); ?></span>
@@ -22,4 +22,27 @@ function sites() {
             ?>
         </div>
     <?php }
+}
+
+function features() {
+    $yaml = new Alchemy\Component\Yaml\Yaml();
+    $data = $yaml->load( ( file_exists( '/vagrant/sandbox-custom.yml' ) ) ? '/vagrant/sandbox-custom.yml' : '/vagrant/sandbox-setup.yml' );
+
+    foreach ( $data['utilities'] as $name => $core ) {
+        if ( ! empty( $core ) ) {
+            foreach ( $core as $feature ) {
+                ?>
+                <li>
+                    <?php
+                    if ( 'phpmyadmin' === $feature ) {
+                        echo '<a href="https://dashboard.test/phpmyadmin">'. $feature . '</a>';
+                    } elseif ( 'tls-ca' === $feature ) {
+                        echo $feature;
+                    }
+                    ?>
+                </li>
+                <?php
+            }
+        }
+    }
 }
--- index.php
+++ index.php
@@ -36,25 +36,7 @@ $data = $yaml->load( ( file_exists( '/vagrant/sandbox-custom.yml' ) ) ? '/vagran
 		<div class="widget-area">
 			<h2 class="widget-title">Additional Features</h2>
 			<ul>
-				<?php
-				foreach ( $data['utilities'] as $name => $core ) {
-					if ( ! empty( $core ) ) {
-						foreach ( $core as $feature ) {
-							?>
-							<li>
-								<?php
-								if ( 'phpmyadmin' === $feature ) {
-									echo '<a href="https://dashboard.test/phpmyadmin">'. $feature . '</a>';
-								} elseif ( 'tls-ca' === $feature ) {
-									echo $feature;
-								}
-								?>
-							</li>
-							<?php
-						}
-					}
-				}
-				?>
+				<?php features(); ?>
 			</ul>
 			<h2 class="widget-title">Bundles</h2>
 			<ul>
