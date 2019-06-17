04a33f53d773dc7882053b6bdf722560c8c96549
Benjamin Lu <benlumia007@gmail.com>
2019-06-17 10:12:34 -0700

    moved scripts

--- index.php
+++ index.php
@@ -11,9 +11,7 @@
 <!doctype html>
 <html>
 <head>
-<meta charset="UTF-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1" />
-<link rel="profile" href="https://gmpg.org/xfn/11" />
+<?php require_once '/framework/scripts.php'; ?>
 </head>
 <body>
 <div id="container" class="site-container">
