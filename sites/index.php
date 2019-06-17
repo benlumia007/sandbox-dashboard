5a2f24dd6b65642b61aea471ad5f2f2079d0a7e1
Benjamin Lu <benlumia007@gmail.com>
2019-06-17 14:43:34 -0700

    adding sites

--- sites/index.php
+++ sites/index.php
@@ -38,19 +38,10 @@
 	<section id="content" class="site-content">
 		<section class="content-area">
 			<header class="entry-header">
-				<h1 class="entry-title">Welcome to Sandbox 1.0.0</h1>
+				<h1 class="entry-title">Sites</h1>
 			</header>
 			<section class="entry-content">
-				<p>
-					Welcome, you have successfully install Sandbox environment. You now full access to all 
-					available features that are included on this system. If you need help, head over to the docs
-					to learn more.
-				</p>
-				<p>
-					Sandbox is a development environment and it should only meant be used for development purposes.
-					It includes Nginx, MySQL, and PHP as its environment and of course, there are other features that
-					are included such as Composer, WP-Cli, and PHPCS for WordPress Coding Standard.
-				</p>
+
 			</section>
 		</section>
 	</section>
