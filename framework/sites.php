ff53a8da2e7a4820b0ef5810f9edc54b7ff8e709
Benjamin Lu <benlumia007@gmail.com>
2019-06-17 15:14:01 -0700

    updated

---
+++ framework/sites.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Dashboard ( index.php )
+ *
+ * @package		Dashboard
+ * @copyright	Copyright (C) 2019. Benjamin Lu
+ * @license		GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
+ * @author		Benjamin Lu ( https://benjlu.com )
+ */
+
+function endsWith( $haystack, $needle ) {
+    $length = strlen( $needle );
+    return $length === 0 ||
+    ( substr( $haystack, -$length ) === $needle );
+}
+
+function sites() {
+    $yaml = new Alchemy\Component\Yaml\Yaml();
+    $data = $yaml->load( ( file_exists( '/vagrant/sandbox-custom.yml' ) ) ? '/vagrant/sandbox-custom.yml' : '/vagrant/sandbox-setup.yml' );
+    foreach ( $data['sites'] as $name => $site ) { ?>
+        <div class="entry-content">
+            <?php
+                if ( ! empty( $site['custom']['title'] ) ) {
+                    $names = $site['custom']['title']; ?>
+                    <h2 class="site-name"><?php echo strip_tags( $names ); ?></h2>
+                <?php } else { ?>
+                    <h2 class="site-name"><?php echo strip_tags( $name ); ?></h2>
+                <?php }
+            ?>
+            <?php
+            if ( ! empty( $site['hosts'] ) ) {
+                foreach ( $site['hosts'] as $host ) { ?>
+                    <a class="link" href="<?php echo 'https://'.$host; ?>" target="_blank"><?php echo 'https://'.$host; ?></a>
+                    <?php
+                }
+            }
+            ?>
+        </div>
+    <?php }
+}
