4b60621a023188526975412c39207d560cea7b38
Benjamin Lu <benlumia007@gmail.com>
2019-06-17 11:28:22 -0700

    updated

--- framework/scripts.php
+++ framework/scripts.php
@@ -13,6 +13,5 @@ function head() {
     printf( '<meta name="viewport" content="width=device-width, initial-scale=1" />' . "\n" );
     printf( '<link rel="profile" href="https://gmpg.org/xfn/11" />' . "\n" );
     printf( '<link href="framework/css/style.css" rel="stylesheet" type="text/css" />' . "\n" );
-    printf( '<link href="framework/css/normalize.css" rel="stylesheet" type="text/css" />' . "\n" );
     printf( '<link href="framework/css/custom-fonts.css" rel="stylesheet" type="text/css" />' . "\n" );
 }
