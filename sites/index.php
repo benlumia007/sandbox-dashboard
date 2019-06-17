8aa7e291a0637d2aac24d10057d5765df178c453
Benjamin Lu <benlumia007@gmail.com>
2019-06-17 14:13:33 -0700

    sites

--- sites/index.php
+++ sites/index.php
@@ -9,6 +9,6 @@
  */
 require_once 'functions.php';
 ?>
-<?php require_once 'header.php'; ?>
+<?php require_once '../header.php'; ?>
 
-<?php require_once 'footer.php'; ?>
+<?php require_once '../footer.php'; ?>
