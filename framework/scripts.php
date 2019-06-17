9d177615f249bcc9076f505871cbbb53004171e7
Benjamin Lu <benlumia007@gmail.com>
2019-06-17 10:12:19 -0700

    initial commit

---
+++ framework/scripts.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Dashboard ( index.php )
+ *
+ * @package		Dashboard
+ * @copyright	Copyright (C) 2019. Benjamin Lu
+ * @license		GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
+ * @author		Benjamin Lu ( https://benjlu.com )
+ */
+
+ function load_scripts() {
+     echo '<meta charset="UTF-8" />';
+     echo '<meta name="viewport" content="width=device-width, initial-scale=1" />';
+     echo '<link rel="profile" href="https://gmpg.org/xfn/11" />';
+ }
