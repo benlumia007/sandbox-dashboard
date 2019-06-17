68f052f380b52d0344228f1706ed3a8076928438
Benjamin Lu <benlumia007@gmail.com>
2019-06-17 09:57:10 -0700

    initial commit

--- index.php
+++ index.php
@@ -1,53 +1,30 @@
+<?php
+/**
+ * Dashboard ( index.php )
+ *
+ * @package		Dashboard
+ * @copyright	Copyright (C) 2019. Benjamin Lu
+ * @license		GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
+ * @author		Benjamin Lu ( https://benjlu.com )
+ */
+?>
 <!doctype html>
 <html>
-	<head>
-		<title>Sandbox WordPress Development</title>
-		<link href="css/style.css" rel="stylesheet" type="text/css" />
-		<link href="css/normalize.css" rel="stylesheet" type="text/css" />
-	</head>
+<head>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<link rel="profile" href="https://gmpg.org/xfn/11" />
+</head>
 <body>
-<?php
-require_once 'includes/yaml.php';
-require_once 'includes/functions.php'; ?>
-
-<section id="container" class="site-container">
+<div id="container" class="site-container">
 	<header id="header" class="site-header">
 		<div class="branding-navigation">
 			<div class="site-branding">
-				<h1 class="site-title">Sandbox</h1>
+				<h1 class="site-title"><a href="https://dashboard.test">Sandbox</a></h1>
 				<h3 class="site-description">WordPress Development</h3>
-			</div>
-			<div class="navigation">
-				<ul>
-					<?php features(); ?>
-				</ul>
 			</div>
 		</div>
 	</header>
-	<section id="content" class="site-content">
-		<div class="content-area">
-			<header class="entry-header">
-				<h1 class="entry-title">WordPress Installation</h1>
-			</header>
-			<div class="content-notification">
-				Below are the sites that were generated, please note that all sites uses secure HTTPS, therefore, you will
-				need to install the TLS-CA certificate within the browser or install it manually, depending on the OS you
-				are using.
-			</div>
-			<div class="box">
-				<?php sites(); ?>
-			</div>
-		</div>
-	</section>
-</section>
-<nav class="secondary">
-	<ul>
-		<li><a href="http://dashboard.test:1080">Mailcatcher</a></li>
-		<li><a href="https://dashboard.test/phpinfo.php">PHP Info</a></li>
-	</ul>
 </div>
-<footer class="footer-area">
-	Copyright (C) 2019. All Rights Reserved
-</footer>
 </body>
 </html>
