4505e5ebda206078c572f513816cc1aaf2442232
Benjamin Lu <benlumia007@gmail.com>
2019-06-17 11:02:26 -0700

    updated

--- framework/scripts.php
+++ framework/scripts.php
@@ -9,7 +9,9 @@
  */
 
  function head() {
-     echo '<meta charset="UTF-8" />';
-     echo '<meta name="viewport" content="width=device-width, initial-scale=1" />';
-     echo '<link rel="profile" href="https://gmpg.org/xfn/11" />';
+     echo '<meta charset="UTF-8" /><br />';
+     echo '<meta name="viewport" content="width=device-width, initial-scale=1" /><br />';
+     echo '<link rel="profile" href="https://gmpg.org/xfn/11" /><br />';
+     echo '<link href="css/style.css" rel="stylesheet" type="text/css" /><br />';
+     echo '<link href="css/normalize.css" rel="stylesheet" type="text/css" />';
  }
