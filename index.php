048ac491df52e8443cee8eb957c05bec789d6fc1
Benjamin Lu <benlumia007@gmail.com>
2019-06-17 11:42:58 -0700

    modify primary navigation

--- index.php
+++ index.php
@@ -24,7 +24,9 @@ require_once 'functions.php';
 			</div>
 			<div class="primary-navigation">
 				<ul>
-					<li><a href="https://dashboard.test">phpMyAdmin</a></li>
+					<li><a href="">Docs</a></li>
+					<li><a href="https://dashboard.test/phpinfo.php">PHP Info</a></li>
+					<li><a href="https://dashboard.test/phpmyadmin">phpMyAdmin</a></li>
 				</ul>
 			</div>
 		</div>
