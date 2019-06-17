1c473a77ab89ff49245a6f962a5fac43f2f963c4
Benjamin Lu <benlumia007@gmail.com>
2019-06-17 14:09:16 -0700

    sites

---
+++ sites/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Dashboard ( index.php )
+ *
+ * @package		Dashboard
+ * @copyright	Copyright (C) 2019. Benjamin Lu
+ * @license		GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
+ * @author		Benjamin Lu ( https://benjlu.com )
+ */
+require_once 'functions.php';
+?>
+<?php require_once 'header.php'; ?>
+
+<?php require_once 'footer.php'; ?>
