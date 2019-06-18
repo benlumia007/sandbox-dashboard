2e4dcea9e865f2fb766df3ddff6492e607a690c1
Benjamin Lu <benlumia007@gmail.com>
2019-06-17 21:50:53 -0700

    content

--- docs/getting-started/index.php
+++ docs/getting-started/index.php
@@ -57,9 +57,11 @@ require_once '../../functions.php';
                     This is one of the requirements that should be installed. Vagrant Hosts Updater allows you
                     to update the hosts file automatically without the need to manually add them.
                 </p>
-                <pre>
-                    vagrant plugin install vagrant-hostsupdater
-                </pre>
+                <p>
+                    <pre>
+                        vagrant plugin install vagrant-hostsupdater
+                    </pre>
+                </p>
             </div>
 		</section>
 	</section>
