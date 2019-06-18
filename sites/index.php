90c85451c6b830d70dbdd112a6da05ff3f501cda
Benjamin Lu <benlumia007@gmail.com>
2019-06-18 16:04:20 -0700

    notifications

--- sites/index.php
+++ sites/index.php
@@ -43,8 +43,12 @@ require_once '../functions.php';
 			<header class="entry-header">
 				<h1 class="entry-title">Sites</h1>
 			</header>
+			<div class="notification">
+				<p>Here are the list of sites that gets generated when you add one or more sites using
+				sandbox-custom.yml</p>
+			</div>
             <div class="box">
-                    <?php sites(); ?>
+				<?php sites(); ?>
             </div>
 		</section>
 	</section>
