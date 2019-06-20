d36280ce07b3afe0df0cfe9eadb83f1e6179857e
Benjamin Lu <benlumia007@gmail.com>
2019-06-20 16:52:10 -0700

    updated

--- docs/new-site/index.php
+++ docs/new-site/index.php
@@ -48,8 +48,7 @@ require_once '../../functions.php';
 					Adding a new site as easy as copying the existing and paste into place and rename. You will then need token_get_all
 					provision so that the new site will be active.
 				</p>
-				<pre>
-				example:
+				<pre>example:
 				  repo: https://github.com/benlumia007/sandbox-custom-site.git
 				  hosts:
 				    - example.test
